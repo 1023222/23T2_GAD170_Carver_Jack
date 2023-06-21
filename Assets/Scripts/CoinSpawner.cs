2338efd9d664ff8872d2a07366c74beb7abe4062
JC <136329097+1023222@users.noreply.github.com>
2023-06-21 16:16:28 +1000

    Update CoinSpawner.cs

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -9,6 +9,8 @@ public class CoinSpawner : MonoBehaviour
     private int numberToSpawn;
     [SerializeField] private int coinPool;
 
+    private bool spawn;
+
     private Vector3 minPosition;
     private Vector3 maxPosition;
 
@@ -17,22 +19,35 @@ public class CoinSpawner : MonoBehaviour
     {
         numberToSpawn = 2;
         coinPool = 0;
+
+        spawn = true;
+
+        Debug.Log("Press Spacebar to spawn two coins. ");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //When player presses spacebar
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //Spawn a coin on object infinitely if space is down
-            //Instantiate(coinPrefab, transform.position, Quaternion.identity, transform);
 
-            //Spawn two coins at random locations
-            Instantiate(coinPrefab, transform.position, Random.rotation, transform);
-            coinPool++;
-            Instantiate(coinPrefab, transform.position, Random.rotation, transform);
-            coinPool++;
+        if (spawn == true)
+        {
+            //When player presses spacebar
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Spawn a coin on object infinitely if space is down
+                //Instantiate(coinPrefab, transform.position, Quaternion.identity, transform);
+
+                //Spawn two coins at random locations
+                Instantiate(coinPrefab, transform.position, Random.rotation, transform);
+                coinPool++;
+                Instantiate(coinPrefab, transform.position, Random.rotation, transform);
+                coinPool++;
+            }    
+            
+            if(coinPool == 2)
+            {
+               spawn = false;
+            }
 
         }
     }
